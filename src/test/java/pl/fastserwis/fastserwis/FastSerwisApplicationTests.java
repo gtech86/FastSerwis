279ff8bdd9848fdb8f8888eb1601945f8a6978fb
gtech86 <pawelp.grabowski@gmail.com>
2022-08-26 12:53:03 +0200

    Initial commit

---
+++ src/test/java/pl/fastserwis/fastserwis/FastSerwisApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fastserwis.fastserwis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastSerwisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
